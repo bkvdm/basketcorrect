a020d931deeefd157eb3282d53f4fad0ea046e80
bkvdm <vadim@991199.ru>
2024-01-03 13:36:35 +0300

    Домашняя работа №25 (исправление) (задание по ссылке: https://my.sky.pro/student-cabinet/stream-lesson/75303/homework-requirements)

---
+++ src/main/java/tel/bvm/basketcorrect/BasketcorrectApplication.java
@@ -0,0 +1,13 @@
+package tel.bvm.basketcorrect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketcorrectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketcorrectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tel/bvm/basketcorrect/BasketcorrectApplicationTests.java
@@ -0,0 +1,13 @@
+package tel.bvm.basketcorrect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketcorrectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
